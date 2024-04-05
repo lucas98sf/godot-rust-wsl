0761e14fece3a1447faccb6cf77105d3297716ba
Lucas Ferreira <lucas.98sf@gmail.com>
2024-04-05 04:58:50 -0300

    add example and hot-reload

---
+++ game/src/lib.rs
@@ -0,0 +1,48 @@
+use godot::engine::{ISprite2D, Sprite2D};
+use godot::prelude::*;
+
+struct MyExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {
+    fn on_level_init(_level: InitLevel) {
+        println!("[Rust]     Init level {:?}", _level);
+    }
+
+    fn on_level_deinit(_level: InitLevel) {
+        println!("[Rust]     Deinit level {:?}", _level);
+    }
+}
+
+#[derive(GodotClass)]
+#[class(base=Sprite2D)]
+struct Player {
+    #[allow(dead_code)]
+    speed: f64,
+    angular_speed: f64,
+
+    base: Base<Sprite2D>,
+}
+
+#[godot_api]
+impl ISprite2D for Player {
+    fn init(base: Base<Sprite2D>) -> Self {
+        godot_print!("Bye!"); // Prints to the Godot console
+
+        Self {
+            speed: 800.0,
+            angular_speed: std::f64::consts::PI,
+            base,
+        }
+    }
+
+    fn physics_process(&mut self, delta: f64) {
+        // In GDScript, this would be:
+        // rotation += angular_speed * delta
+
+        let radians = (self.angular_speed * -delta) as f32;
+        self.base_mut().rotate(radians);
+        // The 'rotate' method requires a f32,
+        // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
+    }
+}
